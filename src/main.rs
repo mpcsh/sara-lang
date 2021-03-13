5394b055904babbc4f94cec5d27f4dc3af93ee55
Mark Cohen <m@mpc.sh>
2021-03-12 22:34:28 -0800

    WIP word-wise scanner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![feature(if_let_guard)]
+
+use std::env::args;
+use std::fs::read_to_string;
+mod scan;
+
 fn main() {
-    println!("Hello, world!");
+    println!();
+
+    let filename = args().last().unwrap();
+    let source_text = read_to_string(filename).unwrap();
+    println!("Source text: {:?}", source_text);
+    println!();
+    
+    let tokens = scan::scan(source_text);
+    println!("Tokens: {:?}", tokens);
+    
+    // scan: [line] -> [token]
+    // parse: [token] -> ast
 }

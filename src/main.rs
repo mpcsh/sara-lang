632afbdc9104a84e36c6c4eeb5be4157007ef7dd
Mark Cohen <m@mpc.sh>
2021-03-14 13:50:59 -0700

    Reformat with tabs

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,25 @@ mod parse;
 mod scan;
 
 fn interpret(source_text: String) -> Result<ast::Recipe, String> {
-    let tokens = scan::scan(source_text)?;
-    println!("Tokens: {:?}", tokens);
-    println!();
+	let tokens = scan::scan(source_text)?;
+	println!("Tokens: {:?}", tokens);
+	println!();
 
-    let recipe = parse::parse(tokens)?;
-    println!("AST: {:?}", recipe);
-    println!();
+	let recipe = parse::parse(tokens)?;
+	println!("AST: {:?}", recipe);
+	println!();
 
-    return Ok(recipe);
+	return Ok(recipe);
 }
 
 fn main() {
-    println!();
+	println!();
 
-    let filename = args().last().unwrap();
-    let source_text = read_to_string(filename).unwrap();
-    println!("Source text: {:?}", source_text);
-    println!();
+	let filename = args().last().unwrap();
+	let source_text = read_to_string(filename).unwrap();
+	println!("Source text: {:?}", source_text);
+	println!();
 
-    let result = interpret(source_text);
-    println!("{:?}", result);
+	let result = interpret(source_text);
+	println!("{:?}", result);
 }

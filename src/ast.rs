88c223cd0c07549eb58c59d3a01b282df338d31e
Mark Cohen <m@mpc.sh>
2021-03-14 13:38:48 -0700

    Implement AST, boilerplate for parser

---
+++ src/ast.rs
@@ -0,0 +1,78 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum Weight {
+    Grams,
+    Kilograms,
+    Ounces,
+    Pounds,
+}
+
+#[derive(Debug)]
+pub enum Volume {
+    Cups,
+    Teaspoons,
+    Tablespoons,
+}
+
+#[derive(Debug)]
+pub enum Temperature {
+    Fahrenheit,
+    Celsius,
+}
+
+#[derive(Debug)]
+pub enum Time {
+    Seconds,
+    Minutes,
+    Hours,
+}
+
+#[derive(Debug)]
+pub enum Unit {
+    Weight,
+    Volume,
+    Temperature,
+    Time,
+}
+
+#[derive(Debug)]
+pub struct Amount {
+    pub quantity: f64,
+    pub unit: Unit,
+}
+
+#[derive(Debug)]
+pub struct Ingredient {
+    pub name: String,
+    pub amount: Amount,
+}
+
+#[derive(Debug)]
+pub enum Instruction {
+    Combine {
+        ingredients: Vec<Ingredient>,
+    },
+    CutInto {
+        source: Ingredient,
+        destination: Ingredient,
+    },
+    Mix {
+        ingredients: Vec<Ingredient>,
+    },
+    Refridgerate {
+        ingredient: Ingredient,
+        time: Time,
+    },
+    Bake {
+        ingredient: Ingredient,
+        temperature: Temperature,
+        time: Time,
+    },
+}
+
+#[derive(Debug)]
+pub struct Recipe {
+    pub ingredients: HashMap<String, Amount>,
+    pub instructions: Vec<Instruction>,
+}
---
+++ src/parse.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+use crate::ast;
+use crate::scan;
+
+pub fn parse(tokens: Vec<scan::Token>) -> Result<ast::Recipe, String> {
+    let recipe = ast::Recipe {
+        ingredients: HashMap::new(),
+        instructions: Vec::new(),
+    };
+
+    return Ok(recipe);
+}
